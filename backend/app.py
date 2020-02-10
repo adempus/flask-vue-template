0e130852df552c4562d5d34126a81d8756ab20fc
adempus <jmorin722@gmail.com>
2020-02-10 15:21:01 -0500

    Modified backend directory structure. Removed node_modules folders.

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,24 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+def getDBCredentials():
+    db_uri = 'mysql://adempus:09sIUDpjhXjnfyJ@localhost/flask_vue_db'
+    with open('./db_credentials.json', 'r') as jsonFile:
+        data = json.load(jsonFile)
+    return f"{data['driver']}://{data['user']}:{data['password']}@{data['host']}:{data['port']}/{data['db']}"
 
 
+# enable debugging
 DEBUG = True
-
 # app instance
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = getDBCredentials()
+
+db = SQLAlchemy(app)
 
 # CORS setup
 CORS(app, resources={ r'/*': {'origins': '*' }})
@@ -19,7 +31,7 @@ def hello_world():
 
 @app.route('/test-route', methods=['GET'])
 def testRoute():
-    return jsonify({'td1': 'first', 'td2': 'second', 'td3': 'third', 'td4': 'fourth'})
+    return jsonify({'td1': 'first', 'td2': 'second', 'td3': 'third', 'td4': 'fourth', 'td5': 'fifth'})
 
 
 if __name__ == '__main__':
